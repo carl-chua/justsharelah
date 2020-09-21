a46de872f22ff91379fbe75d0d84483685e962e9
gemabelanes <E0310795@u.nus.edu>
2020-09-21 22:31:26 +0800

    created API folder

---
+++ src/API/DemoAPI.js
@@ -0,0 +1,66 @@
+import firebase from './Firebase.js'
+
+export function demoPost(setTestData) {
+    firebase.firestore()
+      .collection("test")
+      .add({
+        testData : 'Testing',
+      })
+      .catch(err => {
+        console.log(err);
+      }) 
+  }
+
+export async function demoFetch(setTestData) {
+    var temp = []
+    
+    let snapshot = await firebase.firestore()
+      .collection("test")
+      .get();
+
+    snapshot.forEach((doc) => {
+      temp.push([doc.id,doc.data()]);
+    });
+    setTestData(temp);
+  }
+
+export function demoListener(setTestData) {
+    const unsubscribe = firebase
+    .firestore()
+    .collection("test")
+    .onSnapshot(function (querySnapshot) {
+      querySnapshot.docChanges().forEach(function (changes) {
+        if (changes.type === "added") {
+          setTestData((prevData) => {
+            if (prevData.some((data) => data[0] === changes.doc.id)) {
+              return prevData;
+            } else {
+              return setTestData([...prevData, [changes.doc.id,changes.doc.data()]]);
+            }
+          });
+        } else if (changes.type === "modified") {
+          //console.log('chats modified :', changes.doc.data())
+          setTestData((prevData) =>
+            prevData.map((data) => {
+              //console.log('UPDATING?', message)
+              if (data[0] === changes.doc.id) {
+                return [changes.doc.id,changes.doc.data()];
+              } else {
+                return data;
+              }
+            })
+          );
+        } else if (changes.type === "removed") {
+          setTestData((prevData) => 
+            prevData.map((data) => {
+              if(data[0] !== changes.doc.id) {
+                return data;
+              }
+            })
+          );
+        }
+      });
+    });
+
+  return unsubscribe;
+}
--- src/App.js
+++ src/App.js
@@ -1,79 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import firebase from './Firebase.js'
+import { demoListener, demoPost, demoFetch } from './API/DemoAPI';
 
 function App() {
 
   const[testData, setTestData] = React.useState([]);
 
   function testPost() {
-    firebase.firestore()
-      .collection("test")
-      .add({
-        testData : 'Testing',
-      })
-      .catch(err => {
-        console.log(err);
-      }) 
+    demoPost(setTestData);
   }
 
-  async function testFetch() {
-    var temp = []
-    
-    let snapshot = await firebase.firestore()
-      .collection("test")
-      .get();
-
-    snapshot.forEach((doc) => {
-      temp.push([doc.id,doc.data()]);
-    });
-    setTestData(temp);
+  function testFetch() {
+    demoFetch(setTestData);
   }
 
   React.useEffect(() => {
-    const unsubscribe = firebase
-    .firestore()
-    .collection("test")
-    .onSnapshot(function (querySnapshot) {
-      querySnapshot.docChanges().forEach(function (changes) {
-        if (changes.type === "added") {
-          setTestData((prevData) => {
-            if (prevData.some((data) => data[0] === changes.doc.id)) {
-              return prevData;
-            } else {
-              return setTestData([...prevData, [changes.doc.id,changes.doc.data()]]);
-            }
-          });
-        } else if (changes.type === "modified") {
-          //console.log('chats modified :', changes.doc.data())
-          setTestData((prevData) =>
-            prevData.map((data) => {
-              //console.log('UPDATING?', message)
-              if (data[0] === changes.doc.id) {
-                return [changes.doc.id,changes.doc.data()];
-              } else {
-                return data;
-              }
-            })
-          );
-        } else if (changes.type === "removed") {
-          setTestData((prevData) => 
-            prevData.map((data) => {
-              if(data[0] !== changes.doc.id) {
-                return data;
-              }
-            })
-          );
-        }
-      });
-    });
+    const unsubscribe = demoListener(setTestData);
 
-
-
-
-  return unsubscribe;
-
+    return unsubscribe;
   })
 
   return (
