c789412f4a3420082cb0607884d019110e18c302
MaxChan97 <42763925+MaxChan97@users.noreply.github.com>
2020-09-24 18:09:51 +0800

    Done styling for login page, signup styling still pending

--- src/Components/SignUp.js
+++ src/Components/SignUp.js
@@ -2,6 +2,63 @@ import React, { useCallback } from "react";
 import { withRouter } from "react-router";
 import { Link } from "react-router-dom";
 import firebase from "../API/Firebase";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+import Paper from "@material-ui/core/Paper";
+import Box from "@material-ui/core/Box";
+import Grid from "@material-ui/core/Grid";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import "../Styles/LogIn.css";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    height: "100vh",
+  },
+  image: {
+    backgroundImage: "url(https://source.unsplash.com/random)",
+    backgroundRepeat: "no-repeat",
+    backgroundColor:
+      theme.palette.type === "light"
+        ? theme.palette.grey[50]
+        : theme.palette.grey[900],
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+  },
+  paper: {
+    margin: theme.spacing(8, 4),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    background: "#F5F8F6",
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+    background: "#7AA28A",
+    "&:hover": {
+      backgroundColor: "#7AA28A",
+    },
+  },
+  smalllinks: {
+    color: "#67776D",
+  },
+  input: {
+    borderColor: "#828282",
+    borderWidth: 1,
+    "&:hover": {
+      borderColor: "#828282",
+      borderWidth: 2,
+    },
+  },
+}));
 
 const SignUp = ({ history }) => {
   const handleSignUp = useCallback(
