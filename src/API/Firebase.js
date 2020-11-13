57d1294bb1a2ff0a45411b69ee4c9daaec3c5d8a
gemabelanes <E0310795@u.nus.edu>
2020-11-13 23:41:29 +0800

    change firebase keys

--- src/API/Firebase.js
+++ src/API/Firebase.js
@@ -1,14 +1,21 @@
 import * as firebase from 'firebase';
 
 let config = {
-    apiKey: "AIzaSyCYf-0-MKBW5-N8gA15E9cpKrYLENxbl2g",
+    /*apiKey: "AIzaSyCYf-0-MKBW5-N8gA15E9cpKrYLENxbl2g",
     authDomain: "justsharelah.firebaseapp.com",
     databaseURL: "https://justsharelah.firebaseio.com",
     projectId: "justsharelah",
     storageBucket: "justsharelah.appspot.com",
     messagingSenderId: "344804176098",
     appId: "1:344804176098:web:6e4163aaf6bf97f45deebb",
-    measurementId: "G-9EXSXLX51M"
+    measurementId: "G-9EXSXLX51M"*/
+    apiKey: "AIzaSyCj9UYaqy6t0BtWJkX9c4F55LvWdjfV9lg",
+    authDomain: "is3106g1-b25aa.firebaseapp.com",
+    databaseURL: "https://is3106g1-b25aa.firebaseio.com",
+    projectId: "is3106g1-b25aa",
+    storageBucket: "is3106g1-b25aa.appspot.com",
+    messagingSenderId: "995723633655",
+    appId: "1:995723633655:web:9e02997d0c6d912bf584f2"
   };
 
   firebase.initializeApp(config)
