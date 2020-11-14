5680bd6db9cf3a123b50abac9f7bb38d1ff62f02
nichong99 <e0325243@u.nus.edu>
2020-11-14 13:22:27 +0800

    change pw function

--- src/Components/ChangePassword.js
+++ src/Components/ChangePassword.js
@@ -1,63 +1,72 @@
-import React from 'react'
+import { Box, TextField, Button } from '@material-ui/core';
+import { useAlert } from "react-alert";
+import React from 'react';
 import firebase from "../API/Firebase";
 
-import {Box, Button} from "@material-ui/core";
-import TextField from '@material-ui/core/TextField';
-
 import { makeStyles, useTheme } from '@material-ui/core/styles';
-import "../Styles/Styles.css";
+
 
 const useStyles = makeStyles({
-    root: {
-        marginLeft : "2.5%",
-        marginRight : "2.5%",
-        marginBottom : "5%",
-        borderBottom : "3",
-    },
-    labels: {
-        fontSize: 20,
-        fontWeight: "bold",
-        lineHeight: 2.5,
-    },
-    data: {
-        fontSize: 20,
-    },
-    form: {
-        margin: "2%",
-        width: "60vw",
-    },
-    textfield: {
-        minWidth: "50vw",
-        margin: "3%",
-    },
-    button: {
-        backgroundColor: "#CC7F5D",
-        color: "white",
-        width: "15vw",
-        minHeight: "6vh",
-        marginBottom: "2%",
-    }
+  root: {
+      marginLeft : "2.5%",
+      marginRight : "2.5%",
+      marginBottom : "5%",
+      borderBottom : "3",
+  },
+  labels: {
+      fontSize: 20,
+      fontWeight: "bold",
+      lineHeight: 2.5,
+  },
+  form: {
+      margin: "2%",
+      width: "60vw",
+  },
+  textfield: {
+      minWidth: "50vw",
+      margin: "3%",
+  },
+  button: {
+      backgroundColor: "#CC7F5D",
+      color: "white",
+      width: "15vw",
+      minHeight: "6vh",
+      marginBottom: "2%",
+  }
 })
 
-export default function ChangePassword({user}) {
+export default function ChangePassword() {
 
-    const classes = useStyles()
+  const classes = useStyles()
+  const alert = useAlert();
 
-    const [currPassword, setCurrent] = React.useState("");
-    const handleCurrPassword = (event) => {
-      setCurrent(event.target.value);
-    };
+  
+  const [currPassword, setCurrPassword] = React.useState('');
+  const handleCurrPassword = (event) => {
+    setCurrPassword(event.target.value);
+  };
+  const [newPassword, setNewPassword] = React.useState('');
+  const handleNewPassword = (event) => {
+    setNewPassword(event.target.value);
+  };
 
-    function handleReauthentication() {
-        const emailCred  = firebase.auth.EmailAuthProvider.credential(
-            firebase.auth().currentUser, currPassword);
-            firebase.auth().currentUser.reauthenticateWithCredential(emailCred)
-            .then(() => {
-              // User successfully reauthenticated.
-              const newPass = window.prompt('Please enter new password');
-              return firebase.auth().currentUser.updatePassword(newPass);
-            })
-    }
+  // Reauthenticates the current user and returns a promise...
+  function reauthenticate() {
+    var user = firebase.auth().currentUser;
+    var cred = firebase.auth.EmailAuthProvider.credential(user.email, currPassword);
+    return user.reauthenticateWithCredential(cred);
+  }
+
+
+  // Changes user's password...
+  const onChangePasswordPress = () => {
+    reauthenticate().then(() => {
+      var user = firebase.auth().currentUser;
+      user.updatePassword(newPassword).then(() => {
+        alert.show("Password was changed");
+      }).catch((error) => { console.log(error.message); });
+    });
+  }
 
     return (
         <Box
@@ -70,43 +79,38 @@ export default function ChangePassword({user}) {
                 alignItems: "center"
             }}
         >
-            <form className={classes.form} noValidate autoComplete="off">
-                <div>
-                    <TextField
-                    label="Current Password"
-                    id="outlined-size-small"
-                    onChange={handleCurrPassword}
-                    variant="outlined"
-                    size="small"
-                    className = {classes.textfield}
-                    />
-                </div>
-                <div>
-                    <TextField
-                    label="New Password"
-                    id="outlined-size-small"
-                    variant="outlined"
-                    size="small"
-                    className = {classes.textfield}
-                    />
-                </div>
-                <div>
-                    <TextField
-                    label="Confirm Password"
-                    id="outlined-size-small"
-                    variant="outlined"
-                    size="small"
-                    className = {classes.textfield}
-                    />
-                </div>
-                </form>
-                <Button 
-                    variant="contained" 
-                    className = {classes.button}
-                    onClick = {handleReauthentication}
-                    >
-                    Confirm
-                </Button>   
-        </Box>
-    )
-}
+          <form className={classes.form} noValidate autoComplete="off">        
+            <div>
+            <TextField
+            label="Current Password"
+            id="outlined-size-small"
+            variant="outlined"
+            size="small"
+            className = {classes.textfield}
+            onChange={handleCurrPassword}
+            />
+            </div>
+
+            <div>
+            <TextField
+            label="New Password"
+            id="outlined-size-small"
+            variant="outlined"
+            size="small"
+            className = {classes.textfield}
+            onChange={handleNewPassword}
+            />
+            </div>
+
+        <Button
+          variant="contained"
+          className={classes.button}
+          onClick={onChangePasswordPress}        
+        >
+          Save Changes
+        </Button>
+        </form>
+    
+      </Box>
+    );
+  }
